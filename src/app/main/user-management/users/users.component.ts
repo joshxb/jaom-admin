17298747f3da80c078858487139063340f145f9c
josh <a-joshua@chronostep.com>
2023-10-04 11:54:49 +0800

    revert issues

--- src/app/main/user-management/users/users.component.ts
+++ src/app/main/user-management/users/users.component.ts
@@ -103,7 +103,7 @@ export class UsersComponent implements OnInit, AfterViewInit {
     this.usersManagementService.getAllUserData(page).subscribe((res) => {
       this.isSpinnerLoading = false;
 
-      this.usersData = res;
+      this.usersData = res[0];
       this.filteredUsers = this.usersData?.data;
     });
   }
