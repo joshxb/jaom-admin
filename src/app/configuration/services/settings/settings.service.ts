dff72940622ac7b868edce5dbd5b05fbc1c36be6
josh <144400740+josh-chrono@users.noreply.github.com>
2024-02-21 23:01:12 +0800

    explain here

--- src/app/configuration/services/settings/settings.service.ts
+++ src/app/configuration/services/settings/settings.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
+// Import the AdminService
 import { AdminService } from '../pages/admin.service';
 
 @Injectable({
+  // Provide the service at the root level
   providedIn: 'root',
 })
 export class SettingsService {
+  // Constructor to inject the AdminService
   constructor(private adminService: AdminService) {}
 
+  // Method to update other user data
   updateOtherUserData(user: number, formData: any) {
+    // Delegate the call to the AdminService
     return this.adminService.updateOtherUserData(user, formData);
   }
 }
