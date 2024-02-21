edc58549196a7bccd2cc768260f11eab6b1d1dcd
josh <144400740+josh-chrono@users.noreply.github.com>
2024-02-21 23:01:17 +0800

    explain here

--- src/app/configuration/services/pages/offer.service.ts
+++ src/app/configuration/services/pages/offer.service.ts
@@ -10,49 +10,68 @@ import { Order, ItemsPerPage } from '../../enums/order.enum';
   providedIn: 'root',
 })
 export class OfferService {
+  // Base URL for the API calls
   base = new Base();
+  // Auth service instance
   auth = new AuthService(this.http, this.cookieService);
+  // Base URL for the API calls
   private baseUrl = this.base.baseUrl;
+  // Suffix URL for the API calls
   private suffixUrl = this.base.suffixUrl;
+  // URL for the Offer API calls
   private apiOfferUrl = `${this.baseUrl}/${this.suffixUrl}/offer`;
+  // URL for the Export Offer API calls
   private apiExportOfferUrl = `${this.baseUrl}/${this.suffixUrl}/export/offer`;
 
-  constructor(private http: HttpClient, private cookieService: CookieService) { }
+  constructor(private http: HttpClient, private cookieService: CookieService) {}
 
+  // Method to get the Export Offer data
   getExportOffer(value: number): Observable<any> {
+    // Create headers with Authorization token
     const headers = new HttpHeaders().set(
       'Authorization',
       `Bearer ${this.auth.getToken()}`
     );
 
-    return this.http.get<any>(this.apiExportOfferUrl + `/${value}?role=admin`, {
-      headers,
-    });
+    // Return the HTTP GET request
+    return this.http.get<any>(
+      this.apiExportOfferUrl + `/${value}?role=admin`,
+      { headers }
+    );
   }
 
-  getOffer(page: number, order: Order = Order.Null, items: ItemsPerPage = ItemsPerPage.Null): Observable<any> {
+  // Method to get the Offer data
+  getOffer(
+    page: number,
+    order: Order = Order.Null,
+    items: ItemsPerPage = ItemsPerPage.Null
+  ): Observable<any> {
+    // Create headers with Authorization token
     const headers = new HttpHeaders().set(
       'Authorization',
       `Bearer ${this.auth.getToken()}`
     );
 
-    return this.http.get<any>(this.apiOfferUrl +
-      `?page=${page}` +
-      (order !== Order.Null ? `&order=${order}` : '') +
-      (items !== ItemsPerPage.Null ? `&items=${items}` : '')
-      + `&role=admin`, {
-      headers,
-    });
+    // Return the HTTP GET request
+    return this.http.get<any>(
+      this.apiOfferUrl +
+        `?page=${page}` +
+        (order !== Order.Null ? `&order=${order}` : '') +
+        (items !== ItemsPerPage.Null ? `&items=${items}` : '') +
+        `&role=admin`,
+      { headers }
+    );
   }
 
+  // Method to delete an Offer
   deleteOffer(id: number): Observable<any> {
+    // Create headers with Authorization token
     const headers = new HttpHeaders().set(
       'Authorization',
       `Bearer ${this.auth.getToken()}`
     );
 
+    // Return the HTTP DELETE request
     return this.http.delete<any>(this.apiOfferUrl + `/${id}?role=admin`, {
       headers,
     });
-  }
-}
