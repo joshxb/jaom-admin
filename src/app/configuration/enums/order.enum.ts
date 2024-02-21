6ae3f0375282b474b18a4a1e87902b2f3ab25ad6
josh <144400740+josh-chrono@users.noreply.github.com>
2024-02-21 23:01:12 +0800

    explain here

--- src/app/configuration/enums/order.enum.ts
+++ src/app/configuration/enums/order.enum.ts
@@ -1,14 +1,18 @@
+// This is an exported enum named 'Order' which has three possible values: Null, Asc, and Desc.
+// The 'Null' value is an empty string, 'Asc' represents ascending order, and 'Desc' represents descending order.
 export enum Order {
-  Null = '',
-  Asc = 'asc',
-  Desc = 'desc',
+  Null = '', // 'Null' value is an empty string
+  Asc = 'asc,', // 'Asc' represents ascending order
+  Desc = 'desc', // 'Desc' represents descending order
 }
 
+// This is an exported enum named 'ItemsPerPage' which has six possible values: Null, Five, Ten, Twenty, Fifty, and Hundred.
+// The 'Null' value is an empty string, 'Five' represents five items per page, 'Ten' represents ten items per page, and so on.
 export enum ItemsPerPage {
-  Null = '',
-  Five = '5',
-  Ten = '10',
-  Twenty = '20',
-  Fifty = '50',
-  Hundred = '100',
+  Null = '', // 'Null' value is an empty string
+  Five = '5', // 'Five' represents five items per page
+  Ten = '10', // 'Ten' represents ten items per page
+  Twenty = '20', // 'Twenty' represents twenty items per page
+  Fifty = '50', // 'Fifty' represents fifty items per page
+  Hundred = '100' // 'Hundred' represents one hundred items per page
 }
