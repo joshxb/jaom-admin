6dd54e7bca14f8b5dddd929f499ae544b638ef63
josh <144400740+josh-chrono@users.noreply.github.com>
2024-02-21 23:01:18 +0800

    explain here

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 /// <reference types="@angular/localize" />
 
+// Import the necessary modules and components from Angular
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+// Use the platformBrowserDynamic() method to bootstrap the AppModule
+// This will render the application to the browser
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));
