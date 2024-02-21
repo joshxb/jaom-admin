3180ccd07725e8216cca6f2c2b41e2f2b9344ccd
josh <144400740+josh-chrono@users.noreply.github.com>
2024-02-21 23:01:11 +0800

    explain here

--- src/app/configuration/assets/image.service.ts
+++ src/app/configuration/assets/image.service.ts
@@ -2,19 +2,25 @@ import { Injectable } from '@angular/core';
 import { Ng2ImgMaxService } from 'ng2-img-max';
 import { Observable } from 'rxjs';
 
-@Injectable({ 
+// Image service for compressing images
+@Injectable({
   providedIn: 'root',
 })
 export class ImageService {
+  // Constructor with Ng2ImgMaxService for compressing images
   constructor(private ng2ImgMax: Ng2ImgMaxService) {}
 
+  // Compress image function that takes in a file and returns an observable of the compressed file
   compressImage(image: File): Observable<File> {
     return new Observable<File>((observer) => {
+      // Compress the image with Ng2ImgMaxService
       this.ng2ImgMax.compressImage(image, 0.05).subscribe(
+        // On success, emit the compressed file and complete the observable
         (result) => {
           observer.next(result);
           observer.complete();
         },
+        // On error, emit the error
         (error) => {
           observer.error(error);
         }
