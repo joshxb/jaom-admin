5939688b5e893bab7ed089e82b758c8d9620af3f
josh <144400740+josh-chrono@users.noreply.github.com>
2024-02-21 23:01:09 +0800

    explain here

--- src/app/configuration/services/auth.guard.ts
+++ src/app/configuration/services/auth.guard.ts
@@ -1,27 +1,3 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { AuthService } from './auth.service';
-import { Base, Redirects } from '../configuration.component';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class AuthGuard implements CanActivate {
-  base = new Base();
-  private baseUrl = this.base.baseUrl;
-
-  constructor(private authService: AuthService, private router: Router) {}
-
-  canActivate(): boolean {
-    if (!this.authService.isLoggedIn()) {
-      localStorage.clear();
-
-      if (this.baseUrl == Redirects.localServerUrl) {
-        window.location.href = Redirects.localUserUrl;
-      } else {
-        window.location.href = Redirects.deployUserUrl;
-      }
-    }
-    return true;
-  }
-}
+// Importing CanActivate and Router from @angular/router
+//
