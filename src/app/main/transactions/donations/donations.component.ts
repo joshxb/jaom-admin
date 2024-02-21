bae9718e40f8066034931087522bd594406c838e
josh <144400740+josh-chrono@users.noreply.github.com>
2024-02-21 23:01:37 +0800

    explain here

--- src/app/main/transactions/donations/donations.component.ts
+++ src/app/main/transactions/donations/donations.component.ts
@@ -1,306 +1,135 @@
-import { AfterViewInit, Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { Router, ActivatedRoute } from '@angular/router';
-import { imageUrls } from 'src/app/app.component';
-import { ModalComponent } from '../../modal/modal.component';
-import { TransactionsService } from 'src/app/configuration/services/transactions/transactions.service';
-import { CacheService } from 'src/app/configuration/assets/cache.service';
-import { ImageService } from 'src/app/configuration/services/pages/image.service';
-import { forkJoin } from 'rxjs';
-import { DataName, ExportToExcelService, ExportType } from 'src/app/configuration/assets/export-to-excel.service';
-import { DonationService } from 'src/app/configuration/services/pages/donation.service';
-import { Order, ItemsPerPage } from 'src/app/configuration/enums/order.enum';
+import { 	AfterViewInit, 	Component, 	ElementRef, 	OnInit, 	Render
+er2 	} from '@angular/core';
+import { 	MatDialog 	} from '@angular/material/dialog';
+import { 	Router, 	ActivatedRoute 	} from '@angular/router';
+import { 	imageUrls 	} from 'src/app/app.component';
+import { 	ModalComp 	} from '../../modal/modal.component';
+import { 	TransactionsService 	} from 'src/app/c,onfiguration/services/transactions/transactio
+ns.service';
+import { 	CacheService 	} from 'sr,c/app/configuration/assets/cache.service';
+import { 	ImageService 	} from 'src/app/configuration/services/pages/image.service';
+import { 	forkJoin 	} from 'rxjs';
+import { 	DataName, 	Ex,portToExcelService, 	ExportType 	} from 'src/ap,p/configuration/assets/export-to-excel.servic
+e';
+import { 	DonationService 	} from 'src/app/,configuration/services/pages/donation.service,';
+import { 	Order, 	ItemsPerPage 	} from 'src/a,pp/configuration/enums/order.enum';
 
-@Component({
+@Compone,nt({
   selector: 'app-donations',
-  templateUrl: './donations.component.html',
-  styleUrls: ['./donations.component.css']
+  templateU,rl: './donations.component.html',
+  styleUrls,: ['./donations.component.css']
 })
-export class DonationsComponent implements OnInit, AfterViewInit {
+export cla,ss DonationsComponent implements OnInit, Afte,rViewInit {
+  // Initialize imageUrls object
   imageUrls = new imageUrls();
 
+  // Declare searchTerm variable
   searchTerm: string = '';
-  transactionsData!: any;
+  // Declare transactionsData object
+  transactionsData!,: any;
+  // Declare selectedUser object
   selectedUser: any;
-  filteredTransactions: any[] = [];
-  isSpinnerLoading: boolean = false;
-  showModalSS = false;
-  modalImageUrl: string = '';
+  // Declare filteredTransac,tions array
+  filteredTransac,tions: any[] = [];
+  // Declare isSpinnerLoading variable
+  isSpinnerLoading: boolea,n = false;
+  // Declare showModalSS variable
+  showModalSS: boolean = false;
+  // Declare modalImag,eUrl variable
+  modalImag,eUrl: string = '';
 
-  order: Order = Order.Desc;
+  // Declare order variable
+  order: Order = Order.De,sc;
+  // Declare orderEnum object
   orderEnum = Order;
-  itemEnum = ItemsPerPage;
-  currentPage = 1;
-  itemsPerPage = ItemsPerPage.Ten; //default
+  // Declare itemEnum object
+  itemEnum = ItemsPe,rPage;
+  // Declare currentPage variable
+  currentPage: number = 1;
+  // Declare itemsPerPage variable
+  itemsPerPage: number = It,emsPerPage.Ten; //default
 
-  showConfirmationModal = false;
-  donationToDeleteId!: number;
+  // Declare showConfirmation,Modal variable
+  showConfirmation,Modal: boolean = false;
+  // Declare donationToDeleteId variable
+  donationToDeleteId!: number;,
 
+  // Inject required services in constructor
   constructor(
-    private transactionService: TransactionsService,
-    private router: Router,
+    private transactionServi,ce: TransactionsService,
+    private router: ,Router,
     private route: ActivatedRoute,
     private elRef: ElementRef,
-    private dialog: MatDialog,
+    private dial,og: MatDialog,
     private renderer: Renderer2,
     private elementRef: ElementRef,
     private cacheService: CacheService,
     private imageService: ImageService,
-    private exportToExcelService: ExportToExcelService,
-    private donationService: DonationService
-  ) { }
+    private expo,rtToExcelService: ExportToExcelService,
+    p,rivate donationService: DonationService
+  ) {, }
 
   ngAfterViewInit() {
-    const scb = this.elementRef.nativeElement.querySelector(
+    // Get sidebarCollapseBtn element and add click event listener
+    const scb = thi,s.elementRef.nativeElement.querySelector(
       '#sidebarCollapseBtn'
     );
-    this.renderer.listen(scb, 'click', () => {
-      const sidebar = this.elementRef.nativeElement.querySelector('#sidebar');
+    this.rend,erer.listen(scb, 'click', () => {
+      const, sidebar = this.elementRef.nativeElement.quer,ySelector('#sidebar');
       if (sidebar) {
-        if (sidebar.classList.contains('active')) {
-          this.renderer.removeClass(sidebar, 'active');
-          localStorage.setItem('activeCollapse', JSON.stringify(false));
+        if (sidebar.classList.contains('active,')) {
+          this.renderer.removeClass(sid,ebar, 'active');
+          localStorage.setIt,em('activeCollapse', JSON.stringify(false));
         } else {
-          this.renderer.addClass(sidebar, 'active');
-          localStorage.setItem('activeCollapse', JSON.stringify(true));
+          this.renderer.addC,lass(sidebar, 'active');
+          localStora,ge.setItem('activeCollapse', JSON.stringify(t,rue));
         }
       }
     });
   }
 
-  openConfirmationModal(chat_id: number) {
-    this.isSpinnerLoading = true;
-    setTimeout(() => {
+  openC,onfirmationModal(chat_id: number) {
+    this.,isSpinnerLoading = true;
+    setTimeout(() =>, {
       this.isSpinnerLoading = false;
-      this.donationToDeleteId = chat_id;
-      this.showConfirmationModal = true;
-    }, 1000);
+     , this.donationToDeleteId = chat_id;
+      thi,s.showConfirmationModal = true;
+    }, 1000);,
   }
 
   closeConfirmationModal() {
-    this.showConfirmationModal = false;
+    this.s,howConfirmationModal = false;
   }
 
-  confirmDelete() {
-    this.deleteDonationTransaction(this.donationToDeleteId);
-    this.closeConfirmationModal();
+  confirmD,elete() {
+    this.deleteDonationTransaction(,this.donationToDeleteId);
+    this.closeConfi,rmationModal();
   }
 
   applySearchFilter() {
     if (!this.searchTerm) {
-      this.filteredTransactions = this.transactionsData?.data;
+      this.filter,edTransactions = this.transactionsData?.data;,
     } else {
-      this.filteredTransactions = this.transactionsData?.data.filter(
-        (transacs: { [s: string]: unknown } | ArrayLike<unknown>) =>
-          Object.values(transacs).some((value) => {
-            if (typeof value === 'string') {
-              return value
+      this.filteredTransactions, = this.transactionsData?.data.filter(
+        (transacs: { [s: string]: unknown } | Array,Like<unknown>) =>
+          Object.values(tra,nsacs).some((value) => {
+            if (type,of value === 'string') {
+              return, value
                 .toLowerCase()
-                .includes(this.searchTerm.toLowerCase());
+       ,         .includes(this.searchTerm.toLowerCas,e());
             }
-            if (typeof value === 'number') {
-              return value === parseInt(this.searchTerm.toLowerCase(), 10);
+            if (typeof va,lue === 'number') {
+              return valu,e === parseInt(this.searchTerm.toLowerCase(),, 10);
             }
-            return false;
+            return false;,
           })
       );
     }
   }
 
-  openDialog(s: any, type: string) {
-    const dialogRef = this.dialog.open(ModalComponent, {
-      data: { content: `${s}`, level: type },
-    });
+  openDialo,g(s: any, type: string) {
+    const dialogRef, = this.dialog.open(ModalComponent, {
+      d,ata: { content: `${s}`, level: type },
+    }),;
 
-    dialogRef.afterClosed().subscribe((result) => { });
-  }
-  openModal(imageUrl: string) {
-    this.modalImageUrl = imageUrl;
-    this.showModalSS = true;
-  }
-
-  closeModal() {
-    this.showModalSS = false;
-  }
-
-  ngOnInit(): void {
-    const theme = this.cacheService.getCachedAdminData('theme');
-    this.cacheService.themeChange(this.renderer, this.elRef.nativeElement, theme);
-
-    this.route.queryParams.subscribe((params) => {
-      if (params['page']) {
-        this.currentPage = +params['page'];
-      } else {
-        this.currentPage = 1;
-      }
-
-      if (params['order']) {
-        this.order = params['order'];
-      } else {
-        this.order = Order.Desc;
-      }
-
-      if (params['items']) {
-        this.itemsPerPage = params['items'];
-      } else {
-        this.itemsPerPage = ItemsPerPage.Ten;
-      }
-
-      this.fetchtransactionsData(this.currentPage, this.order, this.itemsPerPage);
-    });
-  }
-
-  fetchtransactionsData(page: number, order: Order = Order.Null, items: ItemsPerPage = ItemsPerPage.Null) {
-    this.isSpinnerLoading = true;
-
-    this.transactionService.getAllPaginatedDonationTransactions(page, order, items).subscribe((res) => {
-      this.isSpinnerLoading = false;
-      this.transactionsData = res;
-      this.filteredTransactions = this.transactionsData?.data;
-
-      const ssDonateObservables = this.filteredTransactions.map((transac) => {
-        return this.imageService.getDonationSSData(transac.id);
-      });
-
-      forkJoin(ssDonateObservables).subscribe((transacs) => {
-        transacs.forEach((imageData, index) => {
-          this.filteredTransactions[index].screenshot_img =
-            URL.createObjectURL(imageData);
-        });
-      });
-    });
-  }
-
-  getPages(): number[] {
-    const totalPages = this.transactionsData?.last_page || 0;
-    return Array.from({ length: totalPages }, (_, index) => index + 1);
-  }
-
-  getCurrentPageEnd(): number {
-    return Math.ceil(this.transactionsData?.total / this.transactionsData?.per_page);
-  }
-
-  onPageChange(page: number, order: Order = Order.Null, items: ItemsPerPage = ItemsPerPage.Null) {
-    this.currentPage = page;
-
-    if (order) {
-      this.order = order;
-    }
-
-    if (items) {
-      this.itemsPerPage = items;
-    }
-
-    this.router.navigate([], {
-      relativeTo: this.route,
-      queryParams: {
-        page: this.currentPage,
-        order: this.order,
-        items: this.itemsPerPage
-      },
-      queryParamsHandling: 'merge',
-    });
-  }
-
-  getStartingIndex(): number {
-    return (this.currentPage - 1) + 1;
-  }
-
-  getPageRange(): number[] {
-    const totalPages = this.transactionsData?.last_page || 0;
-    const displayedPages = Math.min(totalPages, 5);
-    const startPage = Math.max(
-      this.currentPage - Math.floor(displayedPages / 2),
-      1
-    );
-    const endPage = Math.min(startPage + displayedPages - 1, totalPages);
-    return Array.from(
-      { length: endPage - startPage + 1 },
-      (_, index) => startPage + index
-    );
-  }
-
-  deleteDonationTransaction(id: any) {
-    this.isSpinnerLoading = true;
-
-    this.transactionService.deleteDonationTransaction(id).subscribe((res) => {
-      this.isSpinnerLoading = false;
-
-      const deleteDialogMessage = this.elRef.nativeElement.querySelector(
-        '.delete-dialog-message'
-      );
-      const deleteDialogMessageP = this.elRef.nativeElement.querySelector(
-        '.delete-dialog-message p'
-      );
-
-      deleteDialogMessageP.textContent = res?.message;
-      deleteDialogMessage.style.display = 'block';
-
-      setTimeout(() => {
-        window.location.reload();
-      }, 2000);
-    });
-  }
-
-  exportToEXCEL(value: number = 0) {
-    this.isSpinnerLoading = true;
-    const table = document.getElementById('donationTable') as HTMLTableElement;
-
-    if (!table) {
-      console.error("Table element not found");
-      return;
-    }
-
-    let data: any[] = [];
-    for (let i = 1; i < table.rows.length; i++) {
-      const row = table.rows[i];
-
-      if (!row) {
-        console.error("Row element not found");
-        continue;
-      }
-
-      const rowData: { [key: string]: string } = {};
-      for (let j = 0; j < row.cells.length - 1; j++) {
-        const cell = row.cells[j];
-        if (cell.textContent !== null) {
-          const cellText = cell.textContent;
-          rowData[cellText] = cellText;
-        }
-      }
-
-      data.push(rowData);
-    }
-
-    if (value) {
-      this.donationService.getExportDonations(value).subscribe((res) => {
-        let data: any[] = [];
-        const headers: { [key: string]: string | number } = {};
-
-        res.map((item: ArrayLike<unknown> | { [s: string]: unknown; }) => {
-          const newObject: { [key: string]: string | number } = {};
-            Object.entries(item).forEach(([key, value]) => {
-              if (typeof value === 'string') {
-                headers[key] = key;
-                newObject[value] = value;
-              }
-            });
-          data.push(newObject);
-        });
-
-        delete headers['updated_at'];
-        data.unshift(headers);
-
-        setTimeout(() => {
-          this.isSpinnerLoading = false;
-          this.exportToExcelService.exportToExcel(table, data, 'donation-transactions-list', ExportType.Data, DataName.Donation);
-        }, 2000);
-      });
-    } else {
-      setTimeout(() => {
-        this.isSpinnerLoading = false;
-        this.exportToExcelService.exportToExcel(table, data, 'donation-transactions-list', ExportType.Container, DataName.Donation);
-      }, 2000);
-    }
-  }
-}
+    dialogRef.afterClosed().subscribe
