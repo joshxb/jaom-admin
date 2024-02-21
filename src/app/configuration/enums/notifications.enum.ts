7905e38619c3097013f2ea9006ce158626658678
josh <144400740+josh-chrono@users.noreply.github.com>
2024-02-21 23:01:11 +0800

    explain here

--- src/app/configuration/enums/notifications.enum.ts
+++ src/app/configuration/enums/notifications.enum.ts
@@ -1,3 +1,5 @@
+// This is an export statement that makes the enum available for other modules to import
 export enum NotificationEnum {
+  // The enum is called NotificationEnum and contains a single value called ResponseConcernNotification
   ResponseConcernNotification = 'A new message has been replied to your concern',
-  }
+}
