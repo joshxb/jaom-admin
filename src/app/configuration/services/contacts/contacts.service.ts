e06a0cee1e130785af2168eeca7290bad0966a1a
josh <144400740+josh-chrono@users.noreply.github.com>
2024-02-21 23:01:13 +0800

    explain here

--- src/app/configuration/services/contacts/contacts.service.ts
+++ src/app/configuration/services/contacts/contacts.service.ts
@@ -1,24 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ContactService } from '../pages/contact.service';
-import { ItemsPerPage, Order } from '../../enums/order.enum';
+import { ContactService } from '../pages/contact.service'; // Import the ContactService
+import { 
+  ItemsPerPage, 
+  Order 
+} from '../../../enums/order.enum'; // Import the order enumeration
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root' // Provide this service at the root level
 })
 export class ContactsService {
+  // Inject the ContactService into this service
   constructor(
     private contactService: ContactService
   ) {}
 
-  getExportContacts(value: number): Observable<any> {
-    return this.contactService.getExportContacts(value);
+  // Get export contacts from the server
+  getExportContacts(value: any, page: number = 1): Observable<any> {
+    return this.contactService.getExportContacts(value, page);
   }
 
-  getContact(page: number, order: Order = Order.Null, items: ItemsPerPage = ItemsPerPage.Null): Observable<any> {
+  // Get contacts from the server
+  getContact(page: number = 1, order: Order = Order.Null, items: ItemsPerPage = ItemsPerPage.Null): Observable<any> {
     return this.contactService.getContact(page, order, items);
   }
 
+  // Delete a contact from the server
   deleteContact(id: number): Observable<any> {
     return this.contactService.deleteContact(id);
   }
