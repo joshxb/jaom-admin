54a493dab8982af8736d3d5998b5279656928776
josh <144400740+josh-chrono@users.noreply.github.com>
2024-02-21 23:01:16 +0800

    explain here

--- src/app/common/assets/spinner-loading/spinner-loading.component.ts
+++ src/app/common/assets/spinner-loading/spinner-loading.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';  // Importing the Component decorator from Angular's core module
 
-@Component({
-  selector: 'app-spinner-loading',
-  templateUrl: './spinner-loading.component.html',
-  styleUrls: ['./spinner-loading.component.css']
+@Component({   // Defining a new Angular component
+  selector: 'app-spinner-loading',  // Associating the component with a custom HTML element 'app-spinner-loading'
+  templateUrl: './spinner-loading.component.html',  // Specifying the path to the HTML template file for the component
+  styleUrls: ['./spinner-loading.component.css']  // Specifying the path to the CSS stylesheet file for the component
 })
-export class SpinnerLoadingComponent {
+export class SpinnerLoadingComponent {  // Exporting the component class with a name 'SpinnerLoadingComponent'
 
 }
